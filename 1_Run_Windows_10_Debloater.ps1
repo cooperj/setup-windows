06c8120274781d6ad12997b2f34f7826bf20d411
Josh Cooper <joshua@joshuacooper.co.uk>
2020-10-02 10:24:36 +0100

    :fireworks: Add comment

--- 1_Run_Windows_10_Debloater.ps1
+++ 1_Run_Windows_10_Debloater.ps1
@@ -5,5 +5,6 @@
 
 Write-Host "Running Windows 10 Debloater Script..." -ForegroundColor Black -BackgroundColor Yellow
 
+# Run External Script from Git Submodule
 $scriptPath = "$(PWD)\Windows10Debloater\Windows10Debloater.ps1"
 powershell.exe -File $scriptPath
